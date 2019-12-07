3edf37bb4fb3a43463944fc32ab1db0570971c06
Mateusz <mateuszturek005@gmail.com>
2019-12-07 15:54:20 +0100

    Added tests and updated RoofModel class

--- src/main/java/project1/roofequations/model/RoofModel.java
+++ src/main/java/project1/roofequations/model/RoofModel.java
@@ -1,60 +1,84 @@
 package project1.roofequations.model;
 
-public class RoofModel {
+ class RoofModel {
     private Double width;
     private int angle;
+    private double proportion;
 
-    public RoofModel(Double width, int angle) {
-        this.width = width;
-        this.angle = angle;
+    private Double roofHeight ;
+    private Double lengthOfRafter;
+    private Double lowerPartOfRafter;
+    private Double upperPartOfRafter;
+
+     RoofModel() {
     }
 
-    public Double getWidth() {
-        return width;
-    }
-
-    public void setAngle(int angle) {
+    void setAngle(int angle) {
         if (angle == 30 || angle == 45 || angle == 60) {
             this.angle = angle;
         } else
-            this.angle = 0;
+            System.out.println("apropriate angles: 30, 45, 60");
     }
 
-    public int getAngle() {
+     void setWidth(Double width) {
+        if (width > 0)
+        this.width = width;
+    }
+
+     void setProportion() {
+        this.proportion = 0.6;
+    }
+     void setRoofHeight(Double width, int angle) {
+        if (angle == 30){
+            this.roofHeight = width * 0.5 * (Math.sqrt(3) / 3);
+        }
+        if (angle == 45){
+            this.roofHeight = width * 0.5 * 1;
+        }
+        if (angle == 60){
+            this.roofHeight = width * 0.5 * (Math.sqrt(3));
+        }
+    }
+     void setLengthOfRafter(Double width, int angle) {
+        if (angle == 30){
+            this.lengthOfRafter = width / (2 * Math.sqrt(3) / 2);
+        }
+        if (angle == 45){
+            this.lengthOfRafter = width / (2 * Math.sqrt(2) / 2);
+        }
+        if (angle == 60){
+            this.lengthOfRafter = width / 0.5;
+        }
+    }
+
+     void setLowerPartOfRafter(double proportion, Double rafterLength) {
+        this.lowerPartOfRafter = proportion * rafterLength;
+    }
+
+     void setUpperPartOfRafter(Double rafterLength, Double lowerPartOfRafter) {
+        this.upperPartOfRafter = rafterLength - lowerPartOfRafter;
+    }
+
+     int getAngle() {
         return angle;
     }
 
-    public static Double roofHeight(Double width, int angle) {
-        switch (angle) {
-            case 30:
-                return width * 0.5 * (Math.sqrt(3) / 3);
-            case 45:
-                return width * 0.5 * 1;
-            case 60:
-                return width * 0.5 * (Math.sqrt(3));
-        }
-        return null;
+     double getProportion() { return proportion;
     }
 
-    public static Double lengthOfRafter(Double width, int angle) {
-        switch (angle) {
-            case 30:
-                return width / (2 * Math.sqrt(3) / 2);
-            case 45:
-                return width / (2 * Math.sqrt(2) / 2);
-            case 60:
-                return width / 0.5;
-        }
-        return null;
+     Double getRoofHeight() { return roofHeight;
     }
 
-    public static Double lowerPartOfRafter(double proportion, Double rafterLength) {
-        return proportion * rafterLength;
+     Double getLengthOfRafter() { return lengthOfRafter;
     }
 
-    public static Double upperPartOfRafter(Double rafterLength, Double upperPartOfRafter) {
-        return rafterLength - upperPartOfRafter;
+     Double getLowerPartOfRafter() { return lowerPartOfRafter;
     }
 
+     Double getUpperPartOfRafter() { return upperPartOfRafter;
+    }
+
+     Double getWidth() { return width;
+    }
 }
 
--- src/test/java/project1/roofequations/model/RoofModelTest.java
+++ src/test/java/project1/roofequations/model/RoofModelTest.java
@@ -1,50 +1,126 @@
 package project1.roofequations.model;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RoofModelTest {
 
-    RoofModel roofTest = new RoofModel(10000.00, 30);
+    RoofModel roofTest = new RoofModel();
 
     @Test
     public void roofTest() {
-
-        assertEquals(10000.00, roofTest.getWidth());
+        roofTest.setAngle(30);
+        roofTest.setWidth(-4.0);
+        assertNull(roofTest.getWidth());
         assertEquals(30, roofTest.getAngle());
+
+        roofTest.setWidth(10000.00);
+        assertEquals(10000.00,roofTest.getWidth());
+    }
+    @Test
+    public void getRoofProportion(){
+        roofTest.setProportion();
+        assertEquals(0.6,roofTest.getProportion());
+    }
+    @Test
+    void setRoofHeightOther() {
+        roofTest.setRoofHeight(5000.00,20);
+        assertNull(roofTest.getRoofHeight());
+    }
+    @Test
+    void setRoofHeight30degree() {
+        roofTest.setAngle(30);
+        roofTest.setWidth(10000.00);
+        roofTest.setRoofHeight(roofTest.getWidth(),roofTest.getAngle());
+        assertEquals(2886.751345948129,roofTest.getRoofHeight());
+    }
+    @Test
+    void setRoofHeight45degree() {
+        roofTest.setAngle(45);
+        roofTest.setWidth(10000.00);
+        roofTest.setRoofHeight(roofTest.getWidth(),roofTest.getAngle());
+        assertEquals(5000,roofTest.getRoofHeight());
+    }
+    @Test
+    void setRoofHeight60degree() {
+        roofTest.setAngle(60);
+        roofTest.setWidth(10000.00);
+        roofTest.setRoofHeight(roofTest.getWidth(),roofTest.getAngle());
+        assertEquals(8660.254037844386,roofTest.getRoofHeight());
     }
 
     @Test
-    public void roofHeightTest1() {
-        assertEquals(null, RoofModel.roofHeight(roofTest.getWidth(), 0));
-        //assertEquals(2.89,Roof.roofHeight(roofTest.getWidth(),roofTest.getAngle()));
+    void setLengthOfRafterNull() {
+        roofTest.setWidth(10000.00);
+        roofTest.setAngle(20);
+        roofTest.setLengthOfRafter(roofTest.getWidth(),roofTest.getAngle());
+        assertNull(roofTest.getLengthOfRafter());
     }
 
     @Test
-    public void roofHeightTest2() {
-        assertEquals(5000, RoofModel.roofHeight(roofTest.getWidth(), 45));
+    void setLengthOfRafter30() {
+        roofTest.setWidth(10000.00);
+        roofTest.setAngle(30);
+        roofTest.setLengthOfRafter(roofTest.getWidth(),roofTest.getAngle());
+       assertEquals(5773.502691896258,roofTest.getLengthOfRafter());
     }
 
     @Test
-    public void roofHeightTest3() {
-        assertEquals(2886.751345948129, RoofModel.roofHeight(10000.00, 30));
+    void setLowerPartOfRafter() {
+        roofTest.setWidth(10000.00);
+        roofTest.setAngle(30);
+        roofTest.setLengthOfRafter(roofTest.getWidth(),roofTest.getAngle());
+        roofTest.setProportion();
+        roofTest.setLowerPartOfRafter(roofTest.getProportion(),roofTest.getLengthOfRafter());
+        assertEquals(3464.1016151377544,roofTest.getLowerPartOfRafter());
+
     }
 
     @Test
-    public void lengthOfRafterTest() {
-        assertEquals(5773.502691896258, RoofModel.lengthOfRafter(10000.00, 30));
-    }
-
-    @Test
-    void lowerPartOfRafter() {
-        assertEquals(3464.1016151377544, RoofModel.
-                lowerPartOfRafter(0.6, RoofModel.
-                        lengthOfRafter(10000.00, 30)));
-    }
-
-    @Test
-    void upperPartOfRafter() {
-        assertEquals(2310, RoofModel.upperPartOfRafter(5770.0, 3460.0));
+    void setUpperPartOfRafter() {
+        roofTest.setWidth(10000.00);
+        roofTest.setAngle(30);
+        roofTest.setLengthOfRafter(roofTest.getWidth(),roofTest.getAngle());
+        roofTest.setProportion();
+        roofTest.setLowerPartOfRafter(roofTest.getProportion(),roofTest.getLengthOfRafter());
+        roofTest.setUpperPartOfRafter(roofTest.getLengthOfRafter(),roofTest.getLowerPartOfRafter());
+        assertEquals(2309.401076758503,roofTest.getUpperPartOfRafter());
     }
 }
+
+   // @Test
+   // public void roofHeightTest1() {
+    //    assertEquals(null, RoofModel.roofHeight(roofTest.getWidth(), 0));
+        //assertEquals(2.89,Roof.roofHeight(roofTest.getWidth(),roofTest.getAngle()));
+   // }
+
+    //@Test
+    //public void roofHeightTest2() {
+     //   assertEquals(5000, RoofModel.roofHeight(roofTest.getWidth(), 45));
+    //}
+
+    //@Test
+    //public void roofHeightTest3() {
+      //  assertEquals(2886.751345948129, RoofModel.roofHeight(10000.00, 30));
+    //}
+
+  //  @Test
+   // public void lengthOfRafterTest() {
+   //     assertEquals(5773.502691896258, RoofModel.lengthOfRafter(10000.00, 30));
+   // }
+
+    //@Test
+    //void lowerPartOfRafter() {
+     //   assertEquals(3464.1016151377544, RoofModel.
+      //          lowerPartOfRafter(0.6, RoofModel.
+      //                  lengthOfRafter(10000.00, 30)));
+    //}
+
+    //@Test
+    //void upperPartOfRafter() {
+     //   assertEquals(2310, RoofModel.upperPartOfRafter(5770.0, 3460.0));
+
+
