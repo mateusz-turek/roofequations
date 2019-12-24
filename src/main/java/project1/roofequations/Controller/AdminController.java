a8e7d74ca238eba0d896612ab461c4a1f61b0fad
Mateusz <mateuszturek005@gmail.com>
2019-12-24 13:42:21 +0100

    Added AdminController, created form for admin login

--- src/main/java/project1/roofequations/Controller/AdminController.java
+++ src/main/java/project1/roofequations/Controller/AdminController.java
@@ -1,8 +1,84 @@
 package project1.roofequations.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import project1.roofequations.model.AdminModel;
+import project1.roofequations.repository.AdminRepository;
+
+import java.util.Optional;
 
 @Controller
-public class AdminController {
+public class AdminController implements AdminRepository {
 
+    private AdminRepository adminRepository;
+
+    public AdminController(AdminRepository adminRepository) {
+        this.adminRepository = adminRepository;
+    }
+
+    @GetMapping("/admin")
+    public String adminLogin(){
+        return "AdminLogin";
+    }
+    @GetMapping("/panel")
+    public String panel(@RequestParam(required = false) String login,
+                        @RequestParam(required = false) String password){
+        return "Panel";
+    }
+
+    @Override
+    public <S extends AdminModel> S save(S s) {
+        return null;
+    }
+
+    @Override
+    public <S extends AdminModel> Iterable<S> saveAll(Iterable<S> iterable) {
+        return null;
+    }
+
+    @Override
+    public Optional<AdminModel> findById(Long aLong) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Long aLong) {
+        return false;
+    }
+
+    @Override
+    public Iterable<AdminModel> findAll() {
+        return null;
+    }
+
+    @Override
+    public Iterable<AdminModel> findAllById(Iterable<Long> iterable) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+
+    @Override
+    public void delete(AdminModel adminModel) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends AdminModel> iterable) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
 }
